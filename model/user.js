c02f7806bc6f5debef074a00af37832c1f03c493
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-06-08 00:49:44 +0530

    UNIQUE EMAIL

--- model/user.js
+++ model/user.js
@@ -15,6 +15,7 @@ const User=sequelize.define('userInfo',{
     email:{
         type:Sequelize.STRING,
         allowNull:false,
+        unique:true
     },
     password:{
         type:Sequelize.STRING,
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,14 @@ const user=require('../model/user');
 
 
 router.post('/',(req,res,next)=>{
-    user.create({
+    const email=req.body.email
+    user.findOne({where:{email}})
+    .then(existingUser=>{
+        if(existingUser){
+            const errormessage='Email already exist'
+         res.status(400).send(errormessage)
+        }else{
+                user.create({
         name:req.body.name,
         email:req.body.email,
         password:req.body.password
@@ -17,13 +24,17 @@ router.post('/',(req,res,next)=>{
     .catch(err=>{
         console.log(err)
     })
+        }
+    })
 
+
+    
 })
 router.get('/',(req,res,next)=>{
     user.findAll()
-    .then(result=>[
+    .then(result=>{
         res.json(result)
-    ])
+})
     .catch(err=>{
         console.log(err)
     })
